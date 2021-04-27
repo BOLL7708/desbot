822fff242b58f60a932a16fa7d50939a84a42924
Andreas Aronsson <BOLL@7708.net>
2021-04-27 13:26:52 +0200

    Add generalized websockets helper class

--- src/modules/twitch_pubsub.ts
+++ src/modules/twitch_pubsub.ts
@@ -1,6 +1,6 @@
 class TwitchPubsub {
     private _rewards: IPubsubReward[] = []
-    private _socket: WebSocket;
+    private _socket: WebSockets;
     private _reconnectIntervalHandle: number;
     private _pingIntervalHandle: number;
     private _pingTimestamp: number;
@@ -37,20 +37,66 @@ class TwitchPubsub {
     private onReward(id, data) {
         let reward = this._rewards.find(reward => id == reward.id)
         if(reward != null) reward.callback(data)
-        else console.error(`Reward not found: ${id}`)
+        else console.warn(`Reward not found: ${id}`)
     }
 
     init() {
         this._tts.init();
-        this.startConnectLoop()
+        this._socket = new WebSockets(
+            "wss://pubsub-edge.twitch.tv",
+            30,
+            this.onOpen.bind(this),
+            this.onClose.bind(this),
+            this.onMessage.bind(this)
+        )
+        this._socket.init();
     }
 
-    private startConnectLoop() {
-        this._reconnectIntervalHandle = setInterval(this.connect, 30*1000)
-        this.connect()
+    private onOpen(evt:any) {
+        let config:ITwitchConfig = Config.instance.twitch
+        let payload = {
+            type: "LISTEN",
+            nonce: "7708",
+            data: {
+                topics: [`channel-points-channel-v1.${config.userId}`], // ID should come from Helix user request (huh?)
+                auth_token: config.token
+            }
+        }
+        this._socket.send(JSON.stringify(payload))
+        this._pingIntervalHandle = setInterval(this.ping.bind(this), 4*60*1000) // Ping at least every 5 minutes to keep the connection open
     }
-    private stopConnectLoop() {
-        clearInterval(this._reconnectIntervalHandle)
+
+    private onClose(evt:any) {
+        clearInterval(this._pingIntervalHandle)
+    }
+
+    private onMessage(evt:any) {
+        let data = JSON.parse(evt.data)
+        switch(data.type) {
+            case "MESSAGE": 
+                let payload = JSON.parse(unescape(data?.data?.message))
+                if (payload?.type == "reward-redeemed") {
+                    console.log("Reward redeemed!")
+                    let id = payload?.data?.redemption?.reward?.id ?? null
+                    if(id !== null) this.onReward(id, payload?.data)
+                    else console.log(payload)
+                }
+                break
+            case "RECONNECT": 
+                // Server is doing maintenance or similar and wants us to reconnect
+                this._socket.reconnect()
+                break
+            case "PONG":
+                // If a pong response is older than 10 seconds we are recommended to reconnect
+                if(Date.now() - this._pingTimestamp > 10000) this._socket.reconnect()
+                break
+            case "RESPONSE":
+                console.log(evt.data);
+                break;
+            default:
+                console.log(`Unhandled message: ${data.type}`);
+                break;
+        }  
     }
 
     private ping() {
@@ -59,69 +105,5 @@ class TwitchPubsub {
         }
         this._socket.send(JSON.stringify(payload))
         this._pingTimestamp = Date.now()
-    }
-
-    private connect() {
-        let config:ITwitchConfig = Config.instance.twitch
-        this._socket = new WebSocket("wss://pubsub-edge.twitch.tv")
-	    this._socket.onopen = onOpen.bind(this)
-	    this._socket.onclose = onClose.bind(this)
-	    this._socket.onmessage = onMessage.bind(this)
-	    this._socket.onerror = onError.bind(this)
-
-        function onOpen(evt) {
-            this._connected = true
-            this.stopConnectLoop()
-            console.log("PubSub connected")
-            let payload = {
-                type: "LISTEN",
-                nonce: "7708",
-                data: {
-                    topics: [`channel-points-channel-v1.${config.userId}`], // ID should come from Helix user request (huh?)
-                    auth_token: config.token
-                }
-            }
-            this._socket.send(JSON.stringify(payload))
-            this._pingInterval = setInterval(this.ping.bind(this), 4*60*1000) // Ping at least every 5 minutes to keep the connection open
-        }
-        function onClose(evt) {
-            console.log("PubSub disconnected: starting loop")
-            this._connected = false
-            clearInterval(this._pingInterval)
-            this.startConnectLoop()
-        }
-        function onMessage(evt) {
-            let data = JSON.parse(evt.data)
-            switch(data.type) {
-                case "MESSAGE": 
-                    let payload = JSON.parse(unescape(data?.data?.message))
-                    if (payload?.type == "reward-redeemed") {
-                        console.log("Reward redeemed!")
-                        let id = payload?.data?.redemption?.reward?.id ?? null
-                        if(id !== null) this.onReward(id, payload?.data)
-                        else console.log(payload)
-                    }
-                    break
-                case "RECONNECT": 
-                    // Server is doing maintenance or similar and wants us to reconnect
-                    this._socket.close()
-                    break
-                case "PONG":
-                    // If a pong response is older than 10 seconds we are recommended to reconnect
-                    if(Date.now() - this._pingTimestamp > 10000) this._socket.close()
-                    break
-                case "RESPONSE":
-                    console.log(evt.data);
-                    break;
-                default:
-                    console.log(`Unhandled message: ${data.type}`);
-                    break;
-            }        
-        }
-        function onError(evt) {
-            console.log("PubSub error: " + evt.data);
-            this._socket.close();
-            this.startConnectLoop();
-        }
     }
 }
---
+++ src/modules/websockets.ts
@@ -0,0 +1,80 @@
+class WebSockets {
+    constructor(
+        serverUrl:string, 
+        reconnectIntervalSeconds:number=30,
+        onOpen:Function = () => {},
+        onClose:Function = () => {},
+        onMessage:Function = () => {},
+        onError:Function = () => {}
+    ) {
+        this._serverUrl = serverUrl
+        this._reconnectIntervalSeconds = reconnectIntervalSeconds
+        this._onOpen = onOpen
+        this._onClose = onClose
+        this._onMessage = onMessage
+        this._onError = onError
+    }
+
+    private _socket: WebSocket;
+    private _reconnectIntervalSeconds: number;
+    private _reconnectIntervalHandle: number;
+    private _connected: boolean = false;
+    private _serverUrl: string;
+    public _onOpen: Function;
+    public _onClose: Function;
+    public _onMessage: Function;
+    public _onError: Function;
+    
+    init() {
+        this.startConnectLoop()
+    }
+    send(message:string) {
+        if(this._connected) {
+            this._socket.send(message);
+        } else {
+            console.warn(`${this._serverUrl}: Send failed as not connected`);
+        }
+    }
+    reconnect() {
+        this._socket.close();
+        this.connect();
+    }
+
+    private startConnectLoop() {
+        this._reconnectIntervalHandle = setInterval(this.connect, this._reconnectIntervalSeconds*1000)
+        this.connect()
+    }
+    private stopConnectLoop() {
+        clearInterval(this._reconnectIntervalHandle)
+    }
+
+    private connect() {
+        this._socket = new WebSocket(this._serverUrl)
+	    this._socket.onopen = onOpen.bind(this)
+	    this._socket.onclose = onClose.bind(this)
+	    this._socket.onmessage = onMessage.bind(this)
+	    this._socket.onerror = onError.bind(this)
+
+        function onOpen(evt) {
+            console.log(`${this._serverUrl}: Connected`);
+            this._connected = true
+            this.stopConnectLoop()
+            this._onOpen(evt);
+        }
+        function onClose(evt) {
+            console.warn(`${this._serverUrl}: Disconnected`);
+            this._connected = false
+            this.startConnectLoop()
+            this._onClose(evt);
+        }
+        function onMessage(evt) {
+            this._onMessage(evt);
+        }
+        function onError(evt) {
+            console.error(`${this._serverUrl}:${JSON.parse(evt)}`);
+            this._socket.close();
+            this.startConnectLoop();
+            this._onError(evt);
+        }
+    }
+}
