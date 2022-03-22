f388030eee75a3b9c7bfb6ee9af126bbfbfb18af
Andreas Aronsson <BOLL@7708.net>
2022-03-22 20:07:30 +0100

    Let's change it up
    Replaced repeitive index checking to single include, easier to read this

--- src/modules/image_loader.ts
+++ src/modules/image_loader.ts
@@ -2,7 +2,7 @@ class ImageLoader {
     // TODO: Maybe limit cache to 100 images or something? To avoid memory issues.
     static _imageCache: Record<string, string> = {}
 
-    private static async getBlob(url:string):Promise<Blob> {
+    private static async getBlob(url: string): Promise<Blob> {
         const response = await fetch(`${url}?${Math.random()*100000}`) // This has problems with caching otherwise... hmm.
         const blob = await response.blob()
         console.log(`ImageLoader: Loaded remote blob from -> ${url}`)
@@ -15,7 +15,7 @@ class ImageLoader {
      * @param callback Will be called after finished loading
      * @param useCache Will load/store cached data
      */
-    static async getDataUrl(url:string, useCache:boolean=true):Promise<string> {
+    static async getDataUrl(url: string, useCache: boolean=true): Promise<string> {
         const urlb64 = btoa(url)
         const cache = (useCache && this._imageCache.hasOwnProperty(urlb64)) 
             ? this._imageCache[urlb64] 
@@ -39,7 +39,6 @@ class ImageLoader {
                     if(!this.isImage(header)) {
                         reject(new Error('ImageLoader: Not an image'))
                     } else {
-                        // It was already .png
                         ImageLoader._imageCache[urlb64] = imageb64
                         resolve(imageb64)
                     }
@@ -54,7 +53,9 @@ class ImageLoader {
      * Will check if the header belongs to an image
      * @param header Header to check
      */
-    static isImage(header:string):boolean {
-        return header.indexOf('image/png') != -1 || header.indexOf('image/jpeg') != -1 || header.indexOf('image/jpg') != -1 || header.indexOf('image/gif') != -1 || header.indexOf('image/bmp') != -1
+    static isImage(header: string):boolean {
+        const validImageTypes = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'image/bmp']
+        const match = header.match(/.*:(.*);.*/)
+        return match.length == 2 && validImageTypes.includes(match[1].toLowerCase())
     }
 }
