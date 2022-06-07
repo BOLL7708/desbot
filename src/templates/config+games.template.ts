c3dca27421f82bbab0908302328258990a998100
Andreas Aronsson <BOLL@7708.net>
2022-06-07 19:11:28 +0200

    Add game events config template

---
+++ src/templates/config+games.template.ts
@@ -0,0 +1,15 @@
+Config.eventsForGames = {
+    /*
+    [GamesTemplate.ANY_GAME]: {
+        triggers: {
+            reward: {
+                title: '💰 Reward',
+                cost: 5
+            }
+        },
+        actions: {
+            speech: "This reward has been updated for this game!"
+        }
+    }
+    */
+}
