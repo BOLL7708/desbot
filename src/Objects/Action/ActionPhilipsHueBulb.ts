6057bef993dfd44c43c069bc3b6a3425074b5f7c
Andreas Aronsson <BOLL@7708.net>
2024-01-03 22:54:41 +0100

    Allow PhilipsHueBulb actions to perform a sequence of updates

--- src/Objects/Action/ActionPhilipsHueBulb.ts
+++ src/Objects/Action/ActionPhilipsHueBulb.ts
@@ -12,14 +12,15 @@ export class ActionPhilipsHueBulb extends Action {
     entries: number[]|DataEntries<PresetPhilipsHueBulb> = []
     entries_use = OptionEntryUsage.All
     colorEntries: number[]|DataEntries<PresetPhilipsHueBulbState> = []
-    colorEntries_use = OptionEntryUsage.First
+    colorEntries_use = OptionEntryUsage.All
+    colorEntries_delay = 0
 
     enlist() {
         DataMap.addRootInstance(
             new ActionPhilipsHueBulb(),
             'Trigger Philips Hue bulb changes.',
             {
-                colorEntries: 'The color(s) to set the bulb(s) to, as all bulbs are set in a batch only one color will be used even if you pick something that returns multiples.',
+                colorEntries: 'The color(s) to set the bulb(s) to, if more than one color is used and a positive delay in seconds has been provided, they will automatically be applied in sequence.',
                 entries: 'The bulbs to affect.',
             },
             {
@@ -38,10 +39,16 @@ export class ActionPhilipsHueBulb extends Action {
             call: async (user: IActionUser, nonce: string, index?: number) => {
                 const clone = Utils.clone<ActionPhilipsHueBulb>(this)
                 const ids = ArrayUtils.getAsType(DataUtils.ensureKeyArray(clone.entries) ?? [], clone.entries_use, index)
-                const colors = ArrayUtils.getAsType(DataUtils.ensureDataArray(clone.colorEntries) ?? [], clone.colorEntries_use, index)
-                const color = colors.pop() // No reason to set more than one color at the same time for the same bulb.
-                if(color) {
-                    PhilipsHueHelper.runBulbs(ids, color.brightness, color.hue, color.saturation)
+                let colors = ArrayUtils.getAsType(DataUtils.ensureDataArray(clone.colorEntries) ?? [], clone.colorEntries_use, index)
+                if(colors.length > 0 && ids.length > 0) {
+                    if(this.colorEntries_delay <= 0) colors = [colors[0]] // No reason to set more than one color if there is no delay as that would be at the same time.
+                    let delay = 0
+                    for(const color of colors) {
+                        setTimeout(() => {
+                            PhilipsHueHelper.runBulbs(ids, color.brightness, color.hue, color.saturation)
+                        }, delay)
+                        delay += (this.colorEntries_delay*1000)
+                    }
                 }
             }
         }
