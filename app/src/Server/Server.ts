fd37bca88959311889a0d89dabe24abdc369469f
Andreas Aronsson <BOLL@7708.net>
2024-03-10 10:04:21 +0100

    Add some todo & update gitignore

--- app/src/Server/Server.ts
+++ app/src/Server/Server.ts
@@ -2,5 +2,8 @@ export default class Server {
     public static start() {
         console.log('Server started');
 
+        // Express + EJS
+        // WebSockets (OpenVR2WS, SSSVR, etc)
+        // Discord
     }
 }
